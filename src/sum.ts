714d509d0ecee3f44eb32f67bbb94fea7f483de7
koheisugawra <51276845+suzy1031@users.noreply.github.com>
2025-04-01 21:45:26 +0000

    add function

--- src/sum.ts
+++ src/sum.ts
@@ -1,1 +1,8 @@
 export const sum = (a: number, b: number): number => a + b;
+
+export const divide = (a: number, b: number): number => {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed.");
+  }
+  return a / b;
+};
